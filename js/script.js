823c0947bd74233a98c2805501efe89f27230951
ming-make <yeming.ni@gmail.com>
2024-08-01 22:48:56 +0800

    modify index.html & style.css, add script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ function function_abstract() {
         x.style.display = "none";
     }
 }
+
+// function function_bibtex() {
+//     var x = document.getElementById("bibtex");
+//     if (window.getComputedStyle(x).display === "none") {
+//         x.style.display = "flex";
+//         x.style.setProperty('flex-direction', 'column');
+//         x.style.setProperty('justify-content', 'center');
+//         x.style.setProperty('align-items', 'center');
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
