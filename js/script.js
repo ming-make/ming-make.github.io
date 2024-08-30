7488f8db0dea7738f8c49e96b4d60c01094c778e
Ming <81875037+ming-make@users.noreply.github.com>
2024-08-30 16:30:21 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function function_abstract(AbstractElementId) {
 function function_bibtex(BibtexElementId) {
     var x = document.getElementById(BibtexElementId);
     if (window.getComputedStyle(x).display === "none") {
-        x.style.display = "block";
+        x.style.display = "flex";
         x.style.setProperty('flex-direction', 'column');
         x.style.setProperty('justify-content', 'center');
         x.style.setProperty('align-items', 'center');
